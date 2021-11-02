ddd9623cb4dfb451c487a581a47ff1f08387d2d8
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2021-11-02 14:22:36 +0100

    Add project files.

---
+++ H2ProducerConsumer/ProducerConsumer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace H2ProducerConsumer
+{
+    class ProducerConsumer
+    {
+        int item = 0;
+        int maxamount = 6;
+        int totalcount = 0;
+        object _lock = new object();
+        Random r = new Random(Guid.NewGuid().GetHashCode());
+        public void Add()
+        {
+            while (true)
+            {
+                //Locks
+                Monitor.Enter(_lock);
+                try
+                {
+                    //checks to see if item is equal or more than max required, if true, then wait, else produce
+                    if (item >= maxamount)
+                    {
+                        Console.WriteLine("Full..Producer cant produce anymore");
+                        Monitor.Wait(_lock);
+                    }
+                    item += r.Next(1,6);
+                    Console.WriteLine($"Produced {item} item.....Count: {totalcount}");
+                    Monitor.PulseAll(_lock);
+                    Thread.Sleep(3000);
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+        public void Remove()
+        {
+            while (true)
+            {
+                try
+                {
+                    Monitor.Enter(_lock);
+                    //checks to see if item is empty, if true, wait for production, else consume
+                    if (item <= 0)
+                    {
+                        Console.WriteLine("Production is empty");
+                        Monitor.Wait(_lock);
+                    }
+                    item -= r.Next(1,3);
+                    Console.WriteLine($"Consumed {item} item....Count: {totalcount}");
+                    Monitor.PulseAll(_lock);
+                    Thread.Sleep(3000);
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+                }
+            }
+        }
+    }
+}
---
+++ H2ProducerConsumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace H2ProducerConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of ProducerConsumer
+            ProducerConsumer producerConsumer = new ProducerConsumer();
+            //Creates 2 threads, one for add and one for remove, and then start thread
+            Thread producerThread = new Thread(producerConsumer.Add);
+            Thread consumerThread = new Thread(producerConsumer.Remove);
+            producerThread.Start();
+            consumerThread.Start();
+
+        }
+    }
+}
